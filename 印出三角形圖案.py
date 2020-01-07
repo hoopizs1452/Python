38f22cbe8aaf456dec00017e5096dd4369209dc9
ChenYi Lin <chenyilin@ChenYis-iMac.local>
2020-01-07 17:01:22 +0800

    update

---
+++ 印出三角形圖案.py
@@ -0,0 +1,20 @@
+row = int(input('請輸入行數:'))
+for i in range(row):
+    for j in range(i + 1):
+        print('*', end = '')
+    print()
+
+for i in range(row):
+    for j in range(row):
+        if j < row - i - 1:
+            print(' ', end = '')
+        else:
+            print('*', end = '')
+    print()
+
+for i in range(row):
+    for j in range(row - i - 1):
+        print(' ', end = '')
+    for j in range(2 * i + 1):
+        print('*', end = '')
+    print()
